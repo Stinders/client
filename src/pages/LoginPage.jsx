887d8ce91ca701122863b228f2b0d0ccf581751c
spmac <yourspsinghs@gmail.com>
2024-11-26 11:56:45 -0500

    Appointment App

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { Form, Input } from "antd";
-import { Link } from "react-router-dom";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import Axios from "axios";
 
 const LoginPage = () => {
-  const onLoginHandler = (values) => {
+  const navigate = useNavigate();
+
+  const onLoginHandler = async (values) => {
     console.log(values);
+    try {
+      const res = await Axios.post("/api/v1/user/login", values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successful");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("something went wrong");
+    }
   };
   return (
     <div>
@@ -12,8 +28,8 @@ const LoginPage = () => {
       <h1> Login Page</h1>
       <div className="form-container">
         <Form className="login-form" onFinish={onLoginHandler}>
-          <Form.Item label="Username" name="username">
-            <Input type="username" required />
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
           </Form.Item>
           <Form.Item label="Password" name="password">
             <Input type="password" required />
